be889698210189ed089eeff4cdfae2e66684d7a4
tairanliu <dasltr@126.com>
2020-01-28 22:17:29 +0800

    添加推送相关接口

--- src/main/java/com/corgi/conusmers/PushMessageConsumer.java
+++ src/main/java/com/corgi/conusmers/PushMessageConsumer.java
@@ -1,13 +1,27 @@
 package com.corgi.conusmers;
 
+import com.alibaba.dubbo.common.utils.CollectionUtils;
+import com.alibaba.dubbo.common.utils.StringUtils;
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.corgi.common.CorgiQueueName;
 import com.corgi.common.messages.PushMessage;
 import com.corgi.service.PushService;
+import com.corgi.user.api.CorgiUserFollowService;
+import com.corgi.user.api.CorgiUserMatchService;
+import com.corgi.user.api.CorgiUserService;
+import com.corgi.user.entity.UserProfile;
+import com.corgi.user.entity.UserQuery;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author tairanliu
@@ -16,12 +30,92 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RabbitListener(queues = CorgiQueueName.PUSH_MESSAGE_QUEUE)
 public class PushMessageConsumer {
+
     @Autowired
     private PushService pushService;
+    @Reference
+    private CorgiUserFollowService corgiUserFollowService;
+    @Reference
+    private CorgiUserService corgiUserService;
+    @Reference
+    private CorgiUserMatchService corgiUserMatchService;
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @RabbitHandler
     public void process(PushMessage pushMessage) {
-        log.info("recevied message: " + pushMessage.getMessage());
-        pushService.sendMessage(pushMessage);
+        log.info("received message: " + pushMessage);
+        if (PushMessage.FOLLOW.equals(pushMessage.getType())) {
+            int count = corgiUserFollowService.isFollowed(pushMessage.getSourceUserId(), pushMessage.getTargetUserId());
+            if (count < 2) {
+                pushMessage.setMessage(PushMessage.FOLLOW_MESSAGE);
+            } else {
+                pushMessage.setMessage(PushMessage.MATCH_MESSAGE);
+            }
+            pushService.sendMessage(pushMessage);
+        } else if (PushMessage.MATCH.equals(pushMessage.getType())) {
+            List<String> userIds = getUserProfileList(pushMessage);
+            if (CollectionUtils.isNotEmpty(userIds)) {
+                for (String userId : userIds) {
+                    Double match = corgiUserMatchService.getUserMatch(userId, pushMessage.getSourceUserId());
+                    if (match >= 90) {
+                        String key = "match90sent_" + pushMessage.getSourceUserId() + "_" + userId;
+                        String result = redisTemplate.opsForValue().get(key);
+                        if (StringUtils.isNotEmpty(result)) {
+                            pushMessage.setTargetUserId(userId);
+                            pushService.sendMessage(pushMessage);
+                            redisTemplate.opsForValue().set(key, System.currentTimeMillis() + "", 100L, TimeUnit.DAYS);
+                        }
+                    }
+                }
+            }
+        } else if (PushMessage.ACTIVITY.equals(pushMessage.getType())) {
+            List<UserProfile> userProfiles;
+            int page = 1;
+            int pageSize = 500;
+            while (true) {
+                userProfiles = corgiUserFollowService.getFollowedUserByPage(pushMessage.getSourceUserId(), 0L, page, pageSize);
+                sendBatch(userProfiles, pushMessage);
+                if (CollectionUtils.isEmpty(userProfiles) || userProfiles.size() < pageSize) {
+                    break;
+                }
+            }
+        } else {
+            pushService.sendMessage(pushMessage);
+        }
+
+    }
+
+    private void sendBatch(List<UserProfile> userProfileList, PushMessage pushMessage) {
+        if (CollectionUtils.isEmpty(userProfileList)) {
+            return;
+        }
+
+        List<String> registrationIds = new ArrayList<>();
+        String nowTime = System.currentTimeMillis() + "";
+        for (UserProfile userProfile : userProfileList) {
+            Long time = userProfile.getTime();
+            String key = "activitysent_" + userProfile.getUserId();
+            String sentTime = redisTemplate.opsForValue().get(key);
+            if (StringUtils.isEmpty(sentTime) || Long.valueOf(sentTime) < time) {
+                registrationIds.add(userProfile.getImId());
+                redisTemplate.opsForValue().set(key, nowTime, 100L, TimeUnit.DAYS);
+            }
+        }
+        if (CollectionUtils.isNotEmpty(registrationIds)) {
+            pushService.sendMessage(pushMessage, registrationIds);
+        }
+    }
+
+    private List<String> getUserProfileList(PushMessage pushMessage) {
+        HashMap extra = pushMessage.getExtra();
+        Double lat = Double.valueOf(extra.get("lat").toString());
+        Double lng = Double.valueOf(extra.get("lng").toString());
+        UserQuery userQuery = new UserQuery();
+        userQuery.setLat(lat);
+        userQuery.setLng(lng);
+        userQuery.setUserId(pushMessage.getSourceUserId());
+        userQuery.setRange(5.0);
+        return corgiUserService.getAllNearByUser(userQuery);
     }
 }
--- src/main/java/com/corgi/service/PushService.java
+++ src/main/java/com/corgi/service/PushService.java
@@ -8,15 +8,18 @@ import cn.jpush.api.push.model.Message;
 import cn.jpush.api.push.model.Platform;
 import cn.jpush.api.push.model.PushPayload;
 import cn.jpush.api.push.model.audience.Audience;
+import com.alibaba.dubbo.common.utils.CollectionUtils;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.corgi.common.messages.PushMessage;
 import com.corgi.user.api.CorgiUserService;
 import com.corgi.user.entity.UserLogin;
+import com.corgi.user.entity.UserProfile;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author tairanliu
@@ -29,16 +32,29 @@ public class PushService {
     @Reference
     private CorgiUserService corgiUserService;
 
+
     public void sendMessage(PushMessage pushMessage) {
-        String userId = pushMessage.getTargetUserId();
+        sendMessage(pushMessage, null);
+    }
+
+    public void sendMessage(PushMessage pushMessage, List<String> userIds) {
         String message = pushMessage.getMessage();
         HashMap extras = pushMessage.getExtra();
-        UserLogin userLogin = corgiUserService.getUserLogin(userId);
-        if (userLogin != null && userLogin.getImId() != null) {
-            PushPayload payload = getPayload(userLogin.getImId(), message, extras);
+        PushPayload payload = null;
+        if (CollectionUtils.isEmpty(userIds) && pushMessage.getTargetUserId() != null) {
+            String userId = pushMessage.getTargetUserId();
+            log.info("send to: " + userId);
+            UserLogin userLogin = corgiUserService.getUserLogin(userId);
+            if (userLogin != null && userLogin.getImId() != null) {
+                payload = getPayload(userLogin.getImId(), message, extras);
+            }
+        } else if (CollectionUtils.isNotEmpty(userIds)) {
+            payload = getPayload(userIds, message, extras);
+        }
+        if (payload != null) {
             try {
                 PushResult result = jpushClient.sendPush(payload);
-                log.info("send from {} to {}: result={}", pushMessage.getSourceUserId(), userId, result);
+                log.info("send message: {}, result: {}", pushMessage, result);
             } catch (APIConnectionException e) {
                 e.printStackTrace();
             } catch (APIRequestException e) {
@@ -48,7 +64,7 @@ public class PushService {
     }
 
     private PushPayload getPayload(String registrationId, String message, HashMap extras) {
-        if(extras == null){
+        if (extras == null) {
             extras = new HashMap();
         }
         return PushPayload.newBuilder()
@@ -59,4 +75,15 @@ public class PushService {
                 .build();
     }
 
+    private PushPayload getPayload(List<String> registrationIds, String message, HashMap extras) {
+        if (extras == null) {
+            extras = new HashMap();
+        }
+        return PushPayload.newBuilder()
+                .setPlatform(Platform.all())
+                .setAudience(Audience.registrationId(registrationIds))
+                .setMessage(Message.newBuilder().setMsgContent(message)
+                        .addExtras(extras).build())
+                .build();
+    }
 }
