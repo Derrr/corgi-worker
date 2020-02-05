d98286b247f52cc4864db2d5dde53b951073e3c8
tairanliu <dasltr@126.com>
2020-02-05 18:31:36 +0800

    add log

--- src/main/java/com/corgi/conusmers/PushMessageConsumer.java
+++ src/main/java/com/corgi/conusmers/PushMessageConsumer.java
@@ -47,9 +47,13 @@ public class PushMessageConsumer {
         log.info("received message: " + pushMessage);
         if (PushMessage.FOLLOW.equals(pushMessage.getType())) {
             int count = corgiUserFollowService.isFollowed(pushMessage.getSourceUserId(), pushMessage.getTargetUserId());
+            HashMap extra = new HashMap();
+            extra.put("userId",pushMessage.getSourceUserId());
             if (count < 2) {
+                extra.put("type",PushMessage.FOLLOW_MESSAGE_TYPE);
                 pushMessage.setMessage(PushMessage.FOLLOW_MESSAGE);
             } else {
+                extra.put("type",PushMessage.MATCH_MESSAGE_TYPE);
                 pushMessage.setMessage(PushMessage.MATCH_MESSAGE);
             }
             pushService.sendMessage(pushMessage);
