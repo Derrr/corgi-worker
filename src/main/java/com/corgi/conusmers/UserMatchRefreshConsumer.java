24f1ddfcc8b552543230bf512ba185425a9efdec
tairanliu <dasltr@126.com>
2020-07-10 14:52:21 +0800

    fix bug

--- src/main/java/com/corgi/conusmers/UserMatchRefreshConsumer.java
+++ src/main/java/com/corgi/conusmers/UserMatchRefreshConsumer.java
@@ -64,8 +64,7 @@ public class UserMatchRefreshConsumer {
                     String matchKey = CorgiConstants.getUserMatchKey(userDetail1.getUserId(), userDetail2.getUserId());
                     Long expire = redisTemplate.getExpire(matchKey);
                     if (expire > 0) {
-                        redisTemplate.delete(matchKey);
-                        redisTemplate.opsForValue().set(matchKey, match.toString(), expire);
+                        redisTemplate.opsForValue().set(matchKey, match.toString(), expire - System.currentTimeMillis()/1000,TimeUnit.SECONDS);
                     }
                 }
             } else {
