45f22e9fc39af2a4fce9a179fae15bc8baf53896
liutairan <liu.tr@belle.com.cn>
2021-02-02 15:59:39 +0800

    添加推荐逻辑

--- src/main/java/com/corgi/conusmers/UserFeedRefreshConsumer.java
+++ src/main/java/com/corgi/conusmers/UserFeedRefreshConsumer.java
@@ -4,11 +4,8 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.corgi.common.CorgiConstants;
 import com.corgi.common.CorgiQueueName;
 import com.corgi.common.messages.MatchRefresher;
-import com.corgi.user.api.CorgiFeedService;
-import com.corgi.user.api.CorgiUserMatchService;
-import com.corgi.user.api.CorgiUserService;
-import com.corgi.user.entity.UserDetail;
-import com.corgi.user.entity.UserMatch;
+import com.corgi.user.api.*;
+import com.corgi.user.entity.*;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
@@ -20,7 +17,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -32,9 +33,77 @@ import java.util.concurrent.TimeUnit;
 public class UserFeedRefreshConsumer {
     @Reference
     private CorgiFeedService corgiFeedService;
+    @Reference
+    private CorgiVlogService corgiVlogService;
+    @Reference
+    private CorgiUserRecommendService corgiUserRecommendService;
 
     @RabbitHandler
     public void process(String userId) {
+        if (corgiFeedService.countUnviewFeed(userId) >= 20) {
+            return;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String ctime = sdf.format(new Date());
+        List<CorgiVlog> result = new ArrayList<>();
+        merge(result, recallNewVlog(userId, ctime));
+        merge(result, recallRecommendUser(userId, ctime));
+        merge(result, recallRecommendVlog(userId, ctime));
+        for (CorgiVlog vlog : result) {
+            corgiFeedService.addFeed(buildFeed(vlog, userId));
+        }
+        if (corgiFeedService.countUnviewFeed(userId) < 5) {
+
+        }
     }
 
+    private CorgiFeed buildFeed(CorgiVlog vlog, String userId) {
+        CorgiFeed feed = new CorgiFeed();
+        feed.setFeed(vlog.getActivityId());
+        feed.setUserId(userId);
+        feed.setFeedUserId(vlog.getUserId());
+        return feed;
+    }
+
+    private List<CorgiVlog> recallRecommendVlog(String userId, String ctime) {
+        CorgiVlog recall = new CorgiVlog();
+        recall.setCtime(ctime);
+        recall.setUserId(userId);
+        recall.setType("like");
+        return corgiVlogService.recallVlog(recall, 5);
+    }
+
+    private List<CorgiVlog> recallRecommendUser(String userId, String ctime) {
+        List<UserProfile> userProfiles = corgiUserRecommendService.getRecUser(userId, 100);
+        List<CorgiVlog> vlogs = new ArrayList<>();
+        CorgiVlog recall = new CorgiVlog();
+        recall.setCtime(ctime);
+        recall.setUserId(userId);
+        for (UserProfile userProfile : userProfiles) {
+            vlogs.addAll(corgiVlogService.recallTargetVlog(userProfile.getUserId(), recall, 1));
+            if (vlogs.size() >= 5) {
+                return vlogs;
+            }
+        }
+        return vlogs;
+    }
+
+    private List<CorgiVlog> recallNewVlog(String userId, String ctime) {
+        CorgiVlog recall = new CorgiVlog();
+        recall.setCtime(ctime);
+        recall.setUserId(userId);
+        return corgiVlogService.recallVlog(recall, 2);
+    }
+
+    private List<CorgiVlog> merge(List<CorgiVlog> result, List<CorgiVlog> newVlog) {
+        if (CollectionUtils.isEmpty(newVlog)) {
+            return result;
+        }
+        Random random = new Random();
+        for (CorgiVlog vlog : newVlog) {
+            int index = random.nextInt(result.size() + 1);
+            result.add(index, vlog);
+        }
+        return result;
+    }
 }
