af06eee3729b8fd9d34625ac639642b86ef02a2b
liutairan <liu.tr@belle.com.cn>
2021-06-06 17:56:42 +0800

    add log

--- src/main/java/com/corgi/conusmers/PushMessageConsumer.java
+++ src/main/java/com/corgi/conusmers/PushMessageConsumer.java
@@ -256,7 +256,9 @@ public class PushMessageConsumer {
 
     private void checkDate(String dateId, String userId) {
         CorgiDateApply apply = corgiUserDateService.getUserApply(dateId, userId);
-        log.info("apply: {},{} ", apply.getStatus(), apply.getProgress());
+        if(apply != null) {
+            log.info("apply: {},{} ", apply.getStatus(), apply.getProgress());
+        }
         if (apply == null || !"agree".equals(apply.getStatus()) || !"ongoing".equals(apply.getProgress())) {
             return;
         }
