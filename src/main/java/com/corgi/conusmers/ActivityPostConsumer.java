b5ecff8491ee0e790143f659f1d4b209cc27e800
trliu <trliu@hillinsight.com>
2022-12-02 15:21:53 +0800

    添加加权人员

--- src/main/java/com/corgi/conusmers/ActivityPostConsumer.java
+++ src/main/java/com/corgi/conusmers/ActivityPostConsumer.java
@@ -95,7 +95,7 @@ public class ActivityPostConsumer {
     }
 
     private void onHot(CorgiActivity activity, String lockKey) {
-        if (!redisTemplate.opsForValue().setIfAbsent(lockKey, System.currentTimeMillis() + "", 6L, TimeUnit.HOURS)) {
+        if (!redisTemplate.opsForValue().setIfAbsent(lockKey, System.currentTimeMillis() + "", 2L, TimeUnit.HOURS)) {
             return;
         }
         CorgiVlogHot corgiVlogHot = new CorgiVlogHot();
