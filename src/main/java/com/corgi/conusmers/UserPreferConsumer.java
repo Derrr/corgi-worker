203e80ba09a135f14d1f1eb1bbf6ae0bb3e7f048
trliu <trliu@hillinsight.com>
2023-12-11 11:21:23 +0800

    添加身材聚类

--- src/main/java/com/corgi/conusmers/UserPreferConsumer.java
+++ src/main/java/com/corgi/conusmers/UserPreferConsumer.java
@@ -75,7 +75,7 @@ public class UserPreferConsumer {
                     score = 0.0;
                     addGroup.add(group);
                 }
-                Double groupScore = groupMap.get(group) * groupMap.get(group);
+                Double groupScore = groupMap.get(group);
                 totalScore += groupScore;
                 preferMap.put(group, score + groupScore);
             }
@@ -93,7 +93,7 @@ public class UserPreferConsumer {
             corgiUserRecommendService.addPreferCor(userId, group);
         }
         for (String group : preferMap.keySet()) {
-            corgiUserRecommendService.updatePreferCor(userId, group, Math.sqrt(preferMap.get(group)));
+            corgiUserRecommendService.updatePreferCor(userId, group, preferMap.get(group));
         }
     }
 
