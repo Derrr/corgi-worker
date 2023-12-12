28a73414c04b5a6336ab2e01518339ed5ab08635
trliu <trliu@hillinsight.com>
2023-12-12 11:04:20 +0800

    添加身材聚类

--- src/main/java/com/corgi/conusmers/UserGroupConsumer.java
+++ src/main/java/com/corgi/conusmers/UserGroupConsumer.java
@@ -45,7 +45,7 @@ public class UserGroupConsumer {
     public void process(Channel channel, Message message, RecommendCalculater calculater) {
         String userId = calculater.getUserId();
         log.info("calculating... " + userId);
-        if (StringUtils.isEmpty(userId)) {
+        if (!StringUtils.isEmpty(userId)) {
             return;
         }
         UserDetail userDetail = corgiUserService.getUserDetailBasic(userId);
